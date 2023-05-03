3210463c155e20e2124a8a1f1f98964d207f6705
GarwBow <112127867+GarwBow@users.noreply.github.com>
2023-05-03 10:00:21 -0500

    Update Waitstaff.java

--- Waitstaff.java
+++ Waitstaff.java
@@ -4,19 +4,20 @@ An intern developer accidentaly deleted the implementation of the
 setPayRate method of this class. This class will also need to be debugged. 
 
 Documentation:
-
+This class defines the Payrate and jobtitle and returns them. It returns $10.00 and "waitstaff".
 @param
 @return
 
 */
 public class Waitstaff extends Employee
 {
-   public final static double PAY_RATE == 10.00;
+   public final static double PAY_RATE = 10.00;
    public void setPayRate(double rate)
    {
+     payRate = 10.00;
    }
    public void setJobTitle()
    {
-      jobTitle = "waitstaff"
+      jobTitle = "waitstaff";
    }
 }
